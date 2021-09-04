03a0810422ceed5ef19fa106fd1a7c19409fccd8
FiorellaCrocco <fiorellacrocco@gmail.com>
2021-09-04 01:51:24 +0000

    Terminado y con mejoras

--- src/app.js
+++ src/app.js
@@ -5,7 +5,55 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const palo = ["♦", "♥", "♠", "♣"];
+const valor = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const getRandom = maxCount => {
+  return Math.floor(Math.random() * maxCount);
+};
+
+/* Dejo comentado esto que debí cambiar por conflicto con los colores desde el botón*/
+function mostrarEvento() {
+  let numero = document.querySelector(".numero");
+  let rango = getRandom(valor.length);
+  let rangoPalos = getRandom(palo.length);
+  let palo1 = document.querySelector(".palo-sup");
+  let palo2 = document.querySelector(".palo-inf");
+  /*let claseColor = rangoPalos < 2 ? "rojo" : "negro";*/
+
+  numero.innerHTML = valor[rango];
+  palo1.innerHTML = palo[rangoPalos];
+  /*palo1.className += " " + claseColor;*/
+  palo2.innerHTML = palo[rangoPalos];
+  /*palo2.className += " " + claseColor;*/
+  /*console.log(claseColor);*/
+
+  if (rangoPalos < 2) {
+    palo1.style.color = "red";
+    palo2.style.color = "red";
+  } else {
+    palo1.style.color = "black";
+    palo2.style.color = "black";
+  }
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  mostrarEvento();
+};
+
+document.querySelector(".boton").onclick = function() {
+  mostrarEvento();
 };
